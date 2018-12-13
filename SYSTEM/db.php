365d50fe6d17e9926d7d54a546fc6a2add57a184
enidranreb97 <Penelopeee0897@gmail.com>
2018-12-14 02:44:02 +0800

    final system with matching template A&F dept. devby: gerona,cordero,rendon p.s. scm group overnight 12-13-18

--- SYSTEM/db.php
+++
@@ -1,8 +0,0 @@
-<?php  
-	$conn = mysqli_connect('localhost', 'root', '');
-	 if (!$conn)
-    {
-	 die('Could not connect: ' . mysqli_connect_error());
-	}
-	mysqli_select_db("pms", $conn);
-?>
--- SYSTEM/deletePro.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "myfunctions.php";
-	$link = connection();
-	$id = $_GET['id'];
-
-	$query = "delete from register where pk_ID = $id";
-	$result = mysqli_query($link,$query) or die(mysqli_error($link));
-	header("location:home.php");
-?>
